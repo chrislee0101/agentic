9444190f60be21f2053fe881791e5ef49a12724f
chrislee0101 <chrislee.dev@gmail.com>
2025-12-18 16:11:26 -0700

    args and messages

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+import argparse
 
 load_dotenv()
 api_key = os.environ.get('GEMINI_API_KEY')
 client = genai.Client(api_key=api_key)
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
+
 
 def main():
+    parser = argparse.ArgumentParser(description="Agentic Main Function")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    
+    response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents=messages
+    )
+    
+    if response.usage_metadata is None:
+        raise RuntimeError("API request failed: usage metadata is None")
+    
+    
+    
     print("Hello from agentic!")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
 
 
