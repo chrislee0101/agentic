f06dbb1a83a6d5be4898039e3804d8a40b12c4f1
chrislee0101 <chrislee.dev@gmail.com>
2025-12-19 11:27:35 -0700

    verbose arg

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ client = genai.Client(api_key=api_key)
 def main():
     parser = argparse.ArgumentParser(description="Agentic Main Function")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
     
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
@@ -28,8 +29,10 @@ def main():
     
     
     print("Hello from agentic!")
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt} ")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
 
 
